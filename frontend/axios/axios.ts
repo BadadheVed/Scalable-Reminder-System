2a71c39b3ee014a2f0972081a02789d5095f0a10
Ved Badadhe <vbbadadhe@gmail.com>
2025-09-11 23:45:11 +0530

    Some minor axios changes

--- frontend/axios/axios.ts
+++ frontend/axios/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
+const backendUrl: string =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8000";
+
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: backendUrl,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
+
+console.log("Backend URL:", backendUrl);
